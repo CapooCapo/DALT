ac7c1a5c1a5d4a0f94fd2845758e2837fd4c7ca5
Hazhaz1412 <89542925+Hazhaz1412@users.noreply.github.com>
2024-06-22 11:19:45 +0700

    Delete demo-payment-master directory

--- demo-payment-master/config/zalopay.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  app_id: '2553',
-  key1: 'PcY4iZIKFCIdgZvA6ueMcMHHUbRLYjPL',
-  key2: 'kLtgPl8HHhfvMuDHPwKfgfsY4Ydm9eIz',
-  endpoint: 'https://sb-openapi.zalopay.vn/v2/create',
-  callback_url: 'https://4jjl5xvc-5000.asse.devtunnels.ms/callback',
-};
